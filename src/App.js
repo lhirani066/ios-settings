ae8e7a796e4889631dabddcaaeb829e66a4d0d2b
Kumar Sambhav <krsambhav16@gmail.com>
2022-10-14 02:05:55 +0530

    Added Screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App w-screen h-screen flex flex-row justify-center items-center">
+      <div className="screen w-[300px] h-[550px] shadow-2xl">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
