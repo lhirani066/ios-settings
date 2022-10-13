8ba4baea2d68b1dc04fc7660b063672c8a4c8b8e
Kumar Sambhav <krsambhav16@gmail.com>
2022-10-14 02:15:30 +0530

    Added Settings Section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App w-screen h-screen flex flex-row justify-center items-center">
       <div className="screen w-[300px] h-[550px] shadow-2xl">
-
+        <div className="settings-title w-[100%] mt-10 ml-5 text-xl">
+          Settings
+        </div>
       </div>
     </div>
   );
